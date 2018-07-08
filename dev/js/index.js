6ac37e1cf3c75817ce1a8a809f0e6cd1e77c6678
dspyrydo <dspyrydo@student.unit.ua>
2018-07-08 18:17:28 +0300

    app works

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,11 +1,16 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
-import {createStore } from 'redux';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux';
+import allReducers from './reducers'
+import App from './components/App'
 
-const store = createStore();
+const store = createStore(allReducers);
 
 ReactDOM.render(
-    <h2>Hey now</h2>,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
     document.getElementById('root')
 );
